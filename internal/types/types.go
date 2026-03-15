25be114af9df8111a4dbcacd3433b7ce11a6dedc
aprakasa <prakasa.arya@gmail.com>
2026-03-15 23:21:01 +0700

    feat: add types for session management and request handling

---
+++ internal/types/types.go
@@ -0,0 +1,46 @@
+package types
+
+import (
+	"sync"
+
+	"github.com/go-routeros/routeros/v3"
+)
+
+type SessionWrap struct {
+	Conn     *routeros.Client
+	Mu       sync.Mutex
+	Host     string
+	Username string
+	Password string
+}
+
+type SSEClient struct {
+	Ch   chan []byte
+	once sync.Once
+}
+
+func (c *SSEClient) Close() {
+	c.once.Do(func() { close(c.Ch) })
+}
+
+type ConnectRequest struct {
+	RouterID string `json:"router_id"`
+	Host     string `json:"host"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type DisconnectRequest struct {
+	RouterID string `json:"router_id"`
+}
+
+type RunRequest struct {
+	RouterID string   `json:"router_id"`
+	Args     []string `json:"args"`
+}
+
+type JSONResponse struct {
+	Success bool   `json:"success"`
+	Data    any    `json:"data,omitempty"`
+	Error   string `json:"error,omitempty"`
+}
